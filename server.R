02d799d87b9711bbdb8b3f220955b8bae6a7faa2
beco <aalcocer@gmail.com>
2016-11-30 04:06:09 -0600

    changes for deploy

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(twitteR)
+library(rjson)
 source('init.R')
 
 shinyServer(function(input, output) {
@@ -9,25 +10,36 @@ shinyServer(function(input, output) {
 
   observe({
     output$status <- renderText({"loading… connecting to twitter"})
-    if(user != input$screen_name) {
-      user <- input$screen_name
-      otweets <- userTimeline(user, 200, NULL, NULL, TRUE)
-    }
+
+    user <- input$screen_name
+    otweets <- userTimeline(user, 200, NULL, NULL, TRUE)
+
     output$active_user <- renderText(user)
     output$status <- renderText({"done!"})
 
-    #tweets <- otweets[1:input$num]
-    tweets <- otweets
+    tweets <- otweets[1:input$num]
+
     dates  <- sapply(tweets, function(x) format(as.POSIXct(x$created, origin="1970-01-01"), "%m/%d"))
     hours  <- sapply(tweets, function(x) format(as.POSIXct(x$created, origin="1970-01-01"), "%H"))
-    locs <- sapply(tweets, function(x){
+    locs   <- sapply(tweets, function(x){
       if(!identical(x$latitude, character(0))){
         c(x$latitude, x$longitude, x$text)
       }
     })
     locs <- Filter(Negate(is.null), locs)
-    locsdf <- data.frame(t(matrix(unlist(locs), byrow=T)))
-    colnames(locsdf) <- c("lat", "long", "text")
+    if(length(locs) > 0) {
+      locsdf <- data.frame(t(matrix(unlist(locs), byrow=T)))
+      colnames(locsdf) <- c("lat", "long", "text")
+      output$map <- renderLeaflet({
+        leaflet(data = locsdf) %>%
+          addTiles(
+            urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+            attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+        ) %>%
+        setView(lng = -99.20303, lat = 19.4259, zoom = 18) %>%
+        addMarkers(~long, ~lat, popup = ~as.character(text))
+      })
+    }
 
     print(table(hours))
 
@@ -39,17 +51,10 @@ shinyServer(function(input, output) {
       plot(table(hours), xlab="hora del día", ylab="tweets", main="tweets por hora del día")
     })
 
-    # leaflet(data = locsdf) %>% addTiles() %>% setView(lng = -99.20303, lat = 19.4259, zoom = 18) %>%
-    #  addMarkers(~long, ~lat, popup = ~as.character(text))
+    json_file <- "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAiR6Z38_Wih0WP8haFVDyqlAmq7wLXEyA&location=19.4259,-99.20303&radius=500&types=restaurant&"
+    #json_data <- fromJSON(file=json_file)
 
-    output$map <- renderLeaflet({
-      leaflet(data = locsdf) %>%
-        addTiles(
-          urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
-          attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
-      ) %>%
-      setView(lng = -99.20303, lat = 19.4259, zoom = 18) %>%
-      addMarkers(~long, ~lat, popup = ~as.character(text))
-  })
+    print(json_data)
+
   })
 })
